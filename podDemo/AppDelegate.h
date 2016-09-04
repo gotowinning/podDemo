72d8abf743595a4ce6433ce3f375e1a7a16fe205
gotowinning <gotowinning@163.com>
2016-09-04 16:36:08 +0800

    Initial Commit

---
+++ podDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  podDemo
+//
+//  Created by 陈辉 on 16/9/4.
+//  Copyright © 2016年 陈辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
